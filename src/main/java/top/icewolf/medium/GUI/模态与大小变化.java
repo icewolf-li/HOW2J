1f8cdcfccabcf4984b43a6b331044e15b0ab1023
icewolf-li <190775476@qq.com>
2024-04-27 19:31:35 +0800

    添加模态窗口

--- src/main/java/top/icewolf/medium/GUI/模态与大小变化.java
+++ src/main/java/top/icewolf/medium/GUI/模态与大小变化.java
@@ -1,49 +1,41 @@
 package top.icewolf.medium.GUI;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
 
 public class 模态与大小变化 {
     public static void main(String[] args) {
+        // 创建一个窗口
         JFrame f = new JFrame("模态与大小变化");
+        // 设置大小、位置、关闭按钮作用
         f.setSize(400, 300);
         f.setLocation(100, 100);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setLayout(null);
 
-        // 根据外部窗体实例化JDialog
-        JDialog d = new JDialog(f);
+        // 根据 外部窗体 实例化JDialog并且设置标题
+        JDialog d = new JDialog(f,"模态的对话框");
         // 设置为模态
         d.setModal(true);
-
-        d.setTitle("模态的对话框");
+        // 设置大小和位置
         d.setSize(400, 300);
         d.setLocation(200, 200);
         d.setLayout(null);
-        d.setResizable(true);
 
-        JButton b = new JButton("锁定大小");
+        // 创建一个普通窗体的按钮
+        JButton b = new JButton("打开一个模态窗口");
+        // 设置位置和大小
         b.setBounds(50, 50, 280, 30);
+        // 创建按钮监听事件，用于显示模态窗口
+        b.addActionListener(e -> {d.setVisible(true);});
         f.add(b);
 
-        JButton b1 = new JButton("解锁大小");
-        b1.setBounds(50, 50, 280, 30);
-        d.add(b);
-
-        b.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                d.setResizable(false);
-                d.setVisible(true);
-            }
-        });
-        b1.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                d.setResizable(true);
-                d.setVisible(false);
-            }
-        });
-
+        // 创建一个模态窗体的按钮
+        JButton b1 = new JButton("锁定大小");
+        // 设置位置和大小
+        b1.setBounds(50,50,280,30);
+        // 创建按钮监听事件，用于改变模态窗体的“可变大小”属性
+        b1.addActionListener(e -> {if (d.isResizable()) {d.setResizable(false);b1.setText("解锁大小");} else {d.setResizable(true);b1.setText("锁定大小");}});
+        d.add(b1);
 
         f.setVisible(true);
     }
---
+++ src/test/java/top/icewolf/GUI/TestGUIActionListener.java
@@ -0,0 +1,10 @@
+package top.icewolf.GUI;
+
+import top.icewolf.medium.GUI.GUIActionListener;
+
+public class TestGUIActionListener {
+    public static void main(String[] args) {
+        final GUIActionListener guiActionListener = new GUIActionListener();
+        guiActionListener.start();
+    }
+}
